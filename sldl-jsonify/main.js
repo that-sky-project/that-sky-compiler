df06e8b234325f3542a2c87e5bdba2e087f3b82c
LoftRain <raven72847771@126.com>
2026-06-10 14:39:47 +0800

    ver(jsonify): v1.0.0-beta.1

--- sldl-jsonify/main.js
+++ sldl-jsonify/main.js
@@ -1,7 +1,11 @@
 const { ItaniumResolver, kItaniumTypes } = require("./src/itanium.js");
+const { kItaniumException } = require("./src/exception.js");
 
 module.exports = {
   // ./src/itanium.js
   ItaniumResolver,
-  kItaniumTypes
+  kItaniumTypes,
+
+  // ./src/exception.js
+  kItaniumException,
 };
